fd7f78a7ffd3e9eea67c4739d738c1492705cf3a
jedominguez <78039980+YELO-icy@users.noreply.github.com>
2021-07-06 09:46:56 -0600

    this my commit message

---
+++ JUnitTestA/src/main/java/TheStart.java
@@ -0,0 +1,20 @@
+
+public class TheStart {
+
+	public static void main(String[] args) {
+		float intReturn;
+		
+		int x;
+		int y;
+		int z;
+		int zz;
+		
+		Calculator cla = new Calculator();
+		intReturn = cla.add(4, 5);
+		System.out.println(intReturn);
+		
+		
+
+	}
+
+}
